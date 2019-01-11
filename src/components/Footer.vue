df1e301d575cc0f86102de2ad0e22d41c4d4f3f4
Vinicius Mazzurana de Oliveira <vinicius@mazzurana.com>
2019-01-11 15:45:46 -0200

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,94 @@
+<template>
+  <b-footer color="stylish-color-dark" class="page-footer font-small pt-4 mt-4">
+    <b-container class="text-left">
+      <b-row>
+        <b-col md="6">
+          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">
+            Footer Content
+          </h5>
+          <p>
+            Here you can use rows and columns here to organize your footer
+            content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </b-col>
+        <hr class="clearfix w-100 d-md-none" />
+        <b-col md="2">
+          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Link 1</a></li>
+            <li><a href="#!">Link 2</a></li>
+            <li><a href="#!">Link 3</a></li>
+            <li><a href="#!">Link 4</a></li>
+          </ul>
+        </b-col>
+        <hr class="clearfix w-100 d-md-none" />
+        <b-col md="2">
+          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Link 1</a></li>
+            <li><a href="#!">Link 2</a></li>
+            <li><a href="#!">Link 3</a></li>
+            <li><a href="#!">Link 4</a></li>
+          </ul>
+        </b-col>
+        <hr class="clearfix w-100 d-md-none" />
+        <b-col md="2">
+          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Link 1</a></li>
+            <li><a href="#!">Link 2</a></li>
+            <li><a href="#!">Link 3</a></li>
+            <li><a href="#!">Link 4</a></li>
+          </ul>
+        </b-col>
+      </b-row>
+    </b-container>
+    <hr />
+    <div class="text-center py-3">
+      <ul class="list-unstyled list-inline mb-0">
+        <li class="list-inline-item">
+          <h5 class="mb-1">Register for free</h5>
+        </li>
+        <li class="list-inline-item">
+          <a href="#" class="btn btn-danger btn-rounded">Sign up!</a>
+        </li>
+      </ul>
+    </div>
+    <hr />
+    <div class="text-center">
+      <ul class="list-unstyled list-inline">
+        <li class="list-inline-item">
+          <a class="btn-floating btn-sm btn-fb mx-1"
+            ><i class="fa fa-facebook"> </i
+          ></a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn-floating btn-sm btn-tw mx-1"
+            ><i class="fa fa-twitter"> </i
+          ></a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn-floating btn-sm btn-gplus mx-1"
+            ><i class="fa fa-google-plus"> </i
+          ></a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn-floating btn-sm btn-li mx-1"
+            ><i class="fa fa-linkedin"> </i
+          ></a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn-floating btn-sm btn-dribbble mx-1"
+            ><i class="fa fa-dribbble"> </i
+          ></a>
+        </li>
+      </ul>
+    </div>
+    <div class="footer-copyright text-center py-3">
+      <b-container fluid>
+        &copy; 2018 Copyright:
+        <a href="https://www.bootstrap.com"> bootstrap.com </a>
+      </b-container>
+    </div>
+  </b-footer>
+</template>
--- src/components/Site.vue
+++ src/components/Site.vue
@@ -2,21 +2,25 @@
   <div class="site">
     <menuview></menuview>
     <router-view></router-view>
+    <footer></footer>
   </div>
 </template>
 
 <script>
 import MenuView from "@/components/SiteMenu";
+import Footer from "@/components/Footer";
 export default {
   name: "Site",
 
   components: {
-    menuview: MenuView
+    menuview: MenuView,
+    footer: Footer
   },
-  beforeCreate() { // or create a mixin for this purpose
-    if(styles.__inject__) {
+  beforeCreate() {
+    // or create a mixin for this purpose
+    if (styles.__inject__) {
       styles.__inject__(this.$ssrContext);
     }
-}
-}
+  }
+};
 </script>
